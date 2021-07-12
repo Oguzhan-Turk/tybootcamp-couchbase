5bc8181b84ef379339275d3dc7dc55985e42d492
Oguzhan-Turk <82963697+Oguzhan-Turk@users.noreply.github.com>
2021-07-12 05:11:50 +0300

    Update SellerRepository.java

--- src/main/java/com/tybootcamp/couchbase/repository/SellerRepository.java
+++ src/main/java/com/tybootcamp/couchbase/repository/SellerRepository.java
@@ -1,11 +1,20 @@
 package com.tybootcamp.couchbase.repository;
 
+import com.couchbase.client.java.query.QueryScanConsistency;
 import com.tybootcamp.couchbase.domain.Seller;
+
+import java.util.Optional;
+
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.couchbase.repository.ScanConsistency;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SellerRepository extends CouchbaseRepository<Seller, String> {
 
-  void deleteByName(String name);
+	@ScanConsistency(query = QueryScanConsistency.REQUEST_PLUS)
+	void deleteByName(String name);
+
+	@ScanConsistency(query = QueryScanConsistency.REQUEST_PLUS)
+	Optional<Seller> findByName(String name);
 }
