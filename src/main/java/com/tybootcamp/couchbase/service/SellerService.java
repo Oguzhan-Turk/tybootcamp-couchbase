4b7f332f55f19c7b171d1e3716e191fdc450256c
Oguzhan-Turk <82963697+Oguzhan-Turk@users.noreply.github.com>
2021-07-12 05:12:31 +0300

    Update SellerService.java

--- src/main/java/com/tybootcamp/couchbase/service/SellerService.java
+++ src/main/java/com/tybootcamp/couchbase/service/SellerService.java
@@ -1,43 +1,49 @@
 package com.tybootcamp.couchbase.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
 import com.tybootcamp.couchbase.domain.Product;
 import com.tybootcamp.couchbase.domain.Seller;
 import com.tybootcamp.couchbase.repository.SellerRepository;
-import java.util.List;
-import org.springframework.stereotype.Service;
 
 @Service
 public class SellerService {
 
-  private final SellerRepository sellerRepository;
+	private final SellerRepository sellerRepository;
 
-  public SellerService(SellerRepository sellerRepository) {
-    this.sellerRepository = sellerRepository;
-  }
+	public SellerService(SellerRepository sellerRepository) {
+		this.sellerRepository = sellerRepository;
+	}
 
-  public Seller create(String name) {
-    Seller seller = new Seller(name);
-    return sellerRepository.save(seller);
-  }
+	public Seller create(String name) {
+		Seller seller = new Seller(name);
+		return sellerRepository.save(seller);
+	}
 
-  public Seller findById(String id) {
-    return sellerRepository.findById(id).orElseThrow(() -> new RuntimeException(
-        String.format("Seller not found with id: %s", id))
-    );
-  }
+	public Seller findById(String id) {
+		return sellerRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException(String.format("Seller not found with id: %s", id)));
+	}
 
-  public Seller findByName(String name) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+	public Seller findByName(String name) {
+		return sellerRepository.findById(name)
+				.orElseThrow(() -> new RuntimeException(String.format("Seller not found with name: %s", name)));
+	}
 
-  public void addProductsToSeller(String sellerName, List<Product> products) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+	public void addProductsToSeller(String sellerName, List<Product> products) {
+		Seller seller = this.findByName(sellerName);
+		seller.setProducts(products);
+		sellerRepository.save(seller);
+	}
 
-  public List<Product> getProductsByCategory(String sellerName, String category) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+	public List<Product> getProductsByCategory(String sellerName, String category) {
+		Seller seller = this.findByName(sellerName);
+		List<Product> products = seller.getProducts();
+		return products.stream().filter(product -> product.getCategory().contains(category))
+				.collect(Collectors.toList());
+	}
 }
+
