cace5af84f80ed8a9b3e1d47ff73cbac901cf429
Oguzhan-Turk <82963697+Oguzhan-Turk@users.noreply.github.com>
2021-07-12 05:10:37 +0300

    Update Product.java

--- src/main/java/com/tybootcamp/couchbase/domain/Product.java
+++ src/main/java/com/tybootcamp/couchbase/domain/Product.java
@@ -1,35 +1,51 @@
 package com.tybootcamp.couchbase.domain;
 
+import java.util.List;
+
 import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.core.mapping.Field;
 
 @Document
 public class Product {
 
-  @Field
-  private String name;
+	@Field
+	private String name;
 
-  @Field
-  private Double price;
+	@Field
+	private Double price;
 
-  public Product(String name, Double price) {
-    this.name = name;
-    this.price = price;
-  }
+	private List<String> category;
 
-  public String getName() {
-    return name;
-  }
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public Product() {
 
-  public Double getPrice() {
-    return price;
-  }
+	}
 
-  public void setPrice(Double price) {
-    this.price = price;
-  }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getCategory() {
+		return category;
+	}
+
+	public void setCategory(List<String> category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 }
